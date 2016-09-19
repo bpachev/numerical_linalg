b77b67b3f4bb1d91fc0b1fe0c5cb4c407df717cf
Benjamin Pachev <benjaminpachev@gmail.com>
2016-09-19 17:38:53 -0600

    Finished 12.2

--- polynomial_interpolation.py
+++ polynomial_interpolation.py
@@ -26,9 +26,14 @@ def vandermonde(x, deg=None):
     V = np.empty((n,deg))
     for i in xrange(deg):
         V[:,i] = x**i
-    print V, n, x
 
     return V
+
+def sup_norm(A):
+    """
+    Returns the infinity norm of A, a matrix or vector
+    """
+    return la.norm(A, ord=np.inf)
 
 def mk_A(n,m):
     """
@@ -37,19 +42,36 @@ def mk_A(n,m):
         find the interpolating polynomial at the n evenly spaced points x_1, ..., x_n on [0,1],
         and then evaluate the polynomial at each of the m evenly spaced y_1,...,y_m on [0,1].
     """
-    x = np.linspace(0,1,n)
-    y = np.linspace(0,1,m)
+    x = np.linspace(-1,1,n)
+    y = np.linspace(-1,1,m)
     V_x = vandermonde(x, deg=n)
     V_y = vandermonde(y, deg=n)
     return V_y.dot(la.inv(V_x))
 
 if __name__ == "__main__":
-    #Part b
-    norms = []
-    for n in xrange(1,31):
-        m = 2*n-1
-        norms.append(la.norm(mk_A(n,m), ord=np.inf))
+    dom = range(1,31)
 
-    plt.plot(range(1,31), np.array(norms))
+    As = [mk_A(n, 2*n-1) for n in dom]
+    #Part b
+    norms = [sup_norm(A) for A in As]
+
+    plt.plot(dom, np.array(norms))
     plt.yscale('log')
+    plt.title("infinity norm of A with respect to n (part b)")
     plt.show()
+
+    #part c
+    #by 12.9 from trefethen, the condition number is norm(A) * norm(x)/norm(Ax)
+    #pick x to be the all ones
+    ks = []
+    for i,A in enumerate(As):
+        x = np.ones(i+1)
+        ks.append(norms[i]*sup_norm(x)/sup_norm(A.dot(x)))
+
+    plt.plot(dom, ks)
+    plt.yscale('log')
+    plt.title("infinity condition numbers of interpolating the constant function 1 (part c)")
+    plt.show()
+
+#    print ks[10]
+    
