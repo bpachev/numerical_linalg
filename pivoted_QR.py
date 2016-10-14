57183b43dc5adf7aaa1db506cabfc267f97be3f0
benjamin pachev <benjaminpachev@gmail.com>
2016-10-14 17:14:15 -0600

    Instrumented pivoted QR code with testing.

--- pivoted_QR.py
+++ pivoted_QR.py
@@ -37,9 +37,16 @@ def formQ(W, swaps = None):
         dim = len(v)
         Q[k:,k:] -= 2*np.dot(v.reshape((dim,1)), np.dot(v.reshape((1,dim)).conj(), Q[k:,k:]))
         if swaps is not None:
-            #We multiply on the right by the inverse of the swap matrix, which is just the same as the original swap because swapping twice is the indentity transformation
+            #We multiply on the left by the inverse of the swap matrix, which is just the same as the original swap because swapping twice is the identity transformation
             swap_rows(Q, k, swaps[k])
     return Q
+
+def invert_permutation(p):
+    l = len(p)
+    res = p.copy()
+    for k in xrange(len(p)):
+        res[p[k]] = k
+    return res
 
 def formp(col_swaps):
     """
@@ -47,12 +54,12 @@ def formp(col_swaps):
     """
     n = len(col_swaps)
     p = np.arange(n)
-    for k in reversed(range(n)):
+    for k in range(n):
         t = p[k]
-        p[k] = col_swaps[k]
+        p[k] = p[col_swaps[k]]
         p[col_swaps[k]] = t
-
-    return p
+    
+    return invert_permutation(p)
 
 def house(A):
     """
@@ -73,7 +80,7 @@ def house(A):
         dim = m-k
         #now pick an appropriate v for the Householder reflection
         v =  sign(x[0]) * la.norm(x) * ej(dim) + x
-        v = v/norm(v)  
+        v = v/la.norm(v)  
         W[k:, k] = v
         #Multiply the appropriate submatrix of A by the reflector
         R[k:,k:] -= 2*np.dot(v.reshape((dim, 1)), np.dot(v.reshape((1,dim)).conj(), R[k:,k:]))
@@ -88,7 +95,7 @@ def pivoted_QR(A):
     Arguments:
         A -- A complex valued mxn matrix, m >= n.
     Returns: (p,Q,R)
-        p -- a vector containing the n permuted indices corresponding to the permutation matrix P
+        p -- a vector containing the n permuted indices corresponding to the permutation matrix P with AP = QR
         Q -- an mxn matrix with orthonormal columns
         R -- an nxn upper triangular matrix with abs(R[0,0]) >= abs(R[1,1]) >= ... >= abs(R[-1, -1])        
     """
@@ -96,38 +103,69 @@ def pivoted_QR(A):
     m, n = A.shape
     dtype = A.dtype
     if not dtype == np.complex128: dtype = np.float64
-    R = A[:n,:n].astype(dtype).copy()
+    R = A.astype(dtype).copy()
     W = np.zeros(A.shape, dtype = R.dtype)
     row_swaps = np.zeros(n, dtype=int)
     col_swaps = np.zeros(n, dtype=int)
     for k in xrange(n):
         view = R[k:,k:]
+        #find and store the location of the submatrix entry with largest magnitude
         r, c = np.unravel_index(np.argmax(np.abs(R[k:,k:])), (m-k,n-k))
         row_swaps[k] = r+k
         col_swaps[k] = c+k
+        
+        #swap rows
         tmp_row = view[r].copy()
         view[r] = view[0]
         view[0] = tmp_row
+        
+        #swap columns
         tmp_col = view[:,c].copy()        
         view[:,c] = view[:,0]
         view[:,0] = tmp_col
-
+        
+        #Compute the Householder reflection
         x = view[:,0]
-#        print x
- #       print R
         dim = m-k
-        #now pick an appropriate v for the Householder reflection
         v =  sign(x[0]) * la.norm(x) * ej(dim) + x
         v = v/la.norm(v)   
         W[k:, k] = v
-        #Multiply the appropriate submatrix of A by the reflector
+        
+        #Multiply the submatrix by the reflector
         R[k:,k:] -= 2*np.dot(v.reshape((dim, 1)), np.dot(v.reshape((1,dim)).conj(), R[k:,k:]))
 
     Q = formQ(W, swaps=row_swaps)
+#    print np.allclose(Q.T.dot(Q), np.eye(n))
+    R = R[:n,:n]
     p = formp(col_swaps)
-    return p, Q, R
-    
-A = np.arange(9).reshape((3,3)) + np.eye(3)
-print A           
-p,q,r = pivoted_QR(A)        
-print q.dot(r).T[p].T        
+    return p, Q, R[:n,:n]
+
+
+
+if __name__ == "__main__":
+    max_m = 10
+    its = 1
+    max_m = 3
+    print "We test the pivoted QR algorithm by testing it for a variety of random matrices of all sizes."
+    print "Maximum m={}, maximum tests per dimension {}".format(max_m, its)
+    works = set()
+    fails = set()
+    for m in xrange(3,max_m+1):
+        for n in xrange(3, m+1):
+            for i in xrange(its):
+                A = np.random.random((m,n))
+#                A = np.arange(9).reshape((3,3)) + np.eye(3)
+                p, Q, R = pivoted_QR(A)
+                if not np.allclose(A, Q.dot(R).T[p].T):
+                    fails.add(tuple(p))
+                else: works.add(tuple(p))
+                
+#                    print m,n
+ #                   print A
+  #                  print R                
+   #                 print Q.dot(R)
+#                    raise ValueError("The algorithm FAILED")
+                    
+    print works
+    print fails
+
